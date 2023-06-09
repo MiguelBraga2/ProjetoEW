5ac41a6ad7eb37f7defd2fcf0f481f62ef3d63f6
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-06-09 20:23:02 +0100

    Autenticações completas

--- Authentication/models/user.js
+++ Authentication/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new mongoose.Schema({
+  _id: ObjectId,
   password: String,
   username: String,
   name: String,
