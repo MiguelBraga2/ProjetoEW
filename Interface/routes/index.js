9d70f9658470699d583d203a1f05152ced5ddf58
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-06-09 23:30:04 +0100

    Login e register, falta meter as rotas para o facebook e google e mudar o nome no registo

--- Interface/routes/index.js
+++ Interface/routes/index.js
@@ -1,25 +1,72 @@
 var express = require('express');
 var router = express.Router();
-var axios = require('axios')
+var env = require('../config/env');
+var axios = require('axios');
+var jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-router.get('/', function(req, res) {
-  try{
-    res.status(200).render('index')
+function verificaToken(req, res, next){
+  if(req.cookies && req.cookies.token){
+    next()
   }
-  catch(erro){
-      res.status(520).render('error', {error: erro, message: "Erro na obtenção da pagina"})
-    }
-});
+  else{
+    res.redirect('/login')
+  }
+}
 
-router.get('/register', function(req, res) {
-  var data = new Date().toISOString().substring(0, 16);
-  axios.get('http://localhost:8002/register')
-    .then(dados => {
-      res.status(200).render('index', { clist : dados.data , d: data})
+/* GET home page. */
+router.get('/', function(req, res){
+  if(req.cookies && req.cookies.token){
+    jwt.verify(req.cookies.token, process.env.SECRET_KEY, function(e, payload){
+      if(e){
+        res.render('index')
+      }
+      else{
+        res.render('index', {u: payload})
+      }
     })
-    .catch(erro => {
-      res.status(520).render('error', {error: erro, message: "Erro na obtenção da lista de acordãos"})
+  }
+  else {
+    res.render('index')
+  }
+})
+
+// Login
+router.get('/login', (req, res)=>{
+  res.render('loginForm')
+})
+
+router.post('/login', (req, res) => {
+  axios.post(env.authAcessPoint + '/login', req.body)
+    .then(response => {
+      // colocar o token num cookie e enviar para o cliente
+      res.cookie('token', response.data.token)
+      res.redirect('/')
     })
-});
+    .catch(err => {
+      res.render('error', {error: err})
+    })
+})
+
+router.get('/logout', verificaToken, (req, res)=>{
+  res.cookie('token', "revogado.revogado.revogado")
+  res.redirect('/')
+})
+
+// Register
+
+router.get('/register', (req, res)=>{
+  res.render('register')
+})
+
+router.post('/register', (req, res)=>{
+  axios.post(env.authAcessPoint + '/register', req.body)
+    .then(response => {
+      res.redirect('/')
+    })
+    .catch(err => {
+      res.render('error', {error: err})
+    })
+})
 
 module.exports = router;
