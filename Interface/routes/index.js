ffba46cc80fd43ee1b55f4b527d3363cd61cda3b
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-06-14 11:06:27 +0100

    Rotas divididas

--- Interface/routes/index.js
+++ Interface/routes/index.js
@@ -35,68 +35,6 @@ router.get('/', function(req, res){
     })
 })
 
-// Login
-router.get('/login', (req, res)=>{
-  res.render('loginForm')
-})
-
-router.get('/facebook', (req, res)=>{
-  res.redirect(env.authAcessPoint + '/facebook?returnUrl=http://localhost:8003/')
-})
-
-router.get('/google', (req, res)=>{
-  res.redirect(env.authAcessPoint + '/google?returnUrl=http://localhost:8003/')
-})
-
-router.post('/login', (req, res) => {
-  axios.post(env.authAcessPoint + '/login', req.body)
-    .then(response => {
-      // colocar o token num cookie e enviar para o cliente
-      res.cookie('token', response.data.token)
-      res.redirect('/')
-    })
-    .catch(err => {
-      res.render('error', {error: err})
-    })
-})
-
-router.get('/logout', verificaToken, (req, res)=>{
-  res.cookie('token', "revogado.revogado.revogado")
-  res.redirect('/')
-})
-
-// Register
-
-router.get('/register', (req, res)=>{
-  res.render('register')
-})
-
-router.post('/register', (req, res)=>{
-  axios.post(env.authAcessPoint + '/register', req.body)
-    .then(response => {
-      res.redirect('/')
-    })
-    .catch(err => {
-      res.render('error', {error: err, message: err.message})
-    })
-})
-
-
-router.get('/resetPassword', (req, res)=>{
-  res.render('resetPassword')
-})
-
-router.post('/resetPassword', (req, res)=>{
-  axios.put(env.authAcessPoint + '/' + req.body._id + '/password', req.body)
-    .then(response => { 
-      res.redirect('/')
-    })
-    .catch(err => {
-      res.render('error', {error: err, message: err.message})
-    })
-
-})
-
 // pesquisas
 
 router.get('/pesquisa', (req, res)=>{
--- Interface/routes/users.js
+++ Interface/routes/users.js
@@ -1,9 +1,80 @@
 var express = require('express');
 var router = express.Router();
+var env = require('../config/env');
+var axios = require('axios');
+var jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+function verificaToken(req, res, next){
+  if(req.cookies && req.cookies.token){
+    next()
+  }
+  else{
+    res.redirect('/login')
+  }
+}
+
+
+// Login
+router.get('/login', (req, res)=>{
+  res.render('loginForm')
+})
+
+router.get('/facebook', (req, res)=>{
+  res.redirect(env.authAcessPoint + '/facebook?returnUrl=http://localhost:8003/')
+})
+
+router.get('/google', (req, res)=>{
+  res.redirect(env.authAcessPoint + '/google?returnUrl=http://localhost:8003/')
+})
+
+router.post('/login', (req, res) => {
+  axios.post(env.authAcessPoint + '/login', req.body)
+    .then(response => {
+      // colocar o token num cookie e enviar para o cliente
+      res.cookie('token', response.data.token)
+      res.redirect('/')
+    })
+    .catch(err => {
+      res.render('error', {error: err})
+    })
+})
+
+router.get('/logout', verificaToken, (req, res)=>{
+  res.cookie('token', "revogado.revogado.revogado")
+  res.redirect('/')
+})
+
+// Register
+
+router.get('/register', (req, res)=>{
+  res.render('register')
+})
+
+router.post('/register', (req, res)=>{
+  axios.post(env.authAcessPoint + '/register', req.body)
+    .then(response => {
+      res.redirect('/')
+    })
+    .catch(err => {
+      res.render('error', {error: err, message: err.message})
+    })
+})
+
+
+router.get('/resetPassword', (req, res)=>{
+  res.render('resetPassword')
+})
+
+router.post('/resetPassword', (req, res)=>{
+  axios.put(env.authAcessPoint + '/' + req.body._id + '/password', req.body)
+    .then(response => { 
+      res.redirect('/')
+    })
+    .catch(err => {
+      res.render('error', {error: err, message: err.message})
+    })
+
+})
 
 module.exports = router;
