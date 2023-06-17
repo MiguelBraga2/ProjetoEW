2547005e56eefdd58cdee9cb080f15bbd4caabfb
MiguelBraga2 <mgdsbraga@gmail.com>
2023-06-17 17:18:00 +0100

    Tratamento pronto

--- Interface/fileProcessing/fileProcessing.js
+++ Interface/fileProcessing/fileProcessing.js
@@ -32,6 +32,9 @@ exports.processFile = (file_name, current_id) => {
           // Add _id field to each document with incremental count
           documents.forEach((document, index) => {
             document._id = current_id + index; // Index is being incremented
+
+            processDocument(document)
+
             postDocument(document)
           });
     
@@ -56,6 +59,65 @@ exports.processFile = (file_name, current_id) => {
 /**
  * Processes a document, adding new fields, removing other, formatting, etc
  */
-processDocument = (document) => {
+let processDocument = (document) => {
+  // Iterate over the keys of the document
+  const entries = Object.entries(document);
+  // Campos unificados para listas
+  document['Recorridos'] = []
+  document['Áreas Temáticas'] = []
+  document['Referências de Publicação'] = []
+  let descritores = []
 
+  for (let [key, value] of entries){
+    // Quando há unificação de um campo para outro
+    if (key in synonyms){
+      if (key != synonyms[key]){
+        document[synonyms[key]] = value
+        delete document[key]
+      }
+      if (value == ""){ // Remover os valores nulos
+        delete document[key]
+      }
+      // Parsing de cada descritor (Separar por ;)
+      if (key === 'Descritores'){
+        for(desc in document['Descritores']){
+          let novosDescritores = document['Descritores'][desc].split(';')
+          for(descritor in novosDescritores){
+            descritores.push(novosDescritores[descritor].trim())
+          }
+        }
+        document['Descritores'] = descritores
+      }
+      if (key.startsWith('Recorrido')){
+        document['Recorridos'].push(value)
+        delete document[key]
+      }
+      if (key.startsWith('Área Temática')){
+        let novasAreasTematicas = document[key].split(';')
+        for(novaAreaTematica in novasAreasTematicas){
+          document['Áreas Temáticas'].push(novasAreasTematicas[novaAreaTematica].trim())
+        }
+        delete document[key]
+      }
+      if (key.startsWith('Referência de Publicação') || key.startsWith('Referência Publicação')){
+        document['Referências de Publicação'].push(value)
+        delete document[key]
+      }
+      // Casos em que a "Data da Decisão Sumária" é igual à "Data do Acordão" e o "Nº Único do Processo" é igual ao "Processo
+      if (key === 'Data da Decisão Sumária'){
+        if (value === document['Data do Acordão']){
+          delete document[key]
+        }
+      }
+      // Casos em que o "Nº Único do Processo" é igual ao "Processo"
+      if (key === 'Nº Único do Processo'){
+        if (value === document['Processo']){
+          delete document[key]
+        }
+      }
+    } // Caso essa chave não conste da lista
+    else {
+      delete document[key]
+    }
+  }
 }
