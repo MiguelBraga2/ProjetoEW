7e57891f8ba90f37664b4b92bf7c09245944ba9e
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-07-01 17:03:03 +0100

    Aspetos visuais na listagem dos users

--- Interface/public/js/fetchUsers.js
+++ Interface/public/js/fetchUsers.js
@@ -10,37 +10,33 @@ function solicitation(str, page, limit) {
             data.results.forEach(result => {
                 // Criar o HTML para cada resultado
                 let listItemHTML = `<li class="list-group-item">
-                <div class="card mb -2"> <div class="card-body">
-                <div class="row pb-2">
-                <div class="col-md-6">
-                <h5 class="text-start text-primary"><a href="/users/${result._id}">${result.username || 'N/A'}</a></h1>
-                </div>
-                <div class="col-md-6">`
-
-                listItemHTML +=`</div></div>
-                <div class="row pb-2">
-                    <div class="col-md-4"> 
-                        <div class="text-start"> <b>Username:</b> ${result.username || 'N/A'}</div>
-                    </div>
-                    <div class="col-md-4"> 
-                        <div class="text-start"> <b>Email:</b> ${result.email || 'N/A'}</div>
-                    </div>
-                </div>
-                `
-                if (result.active){
-                    listItemHTML += `<div class="text-end h5"><a class="mx-1" href="/users/disable/${result._id}"><i class="fa-solid fa-pause"></i></a>`
-                }
-                else{
-                    listItemHTML += `<div class="text-end h5"><a class="mx-1" href="/users/enable/${result._id}"><i class="fa-solid fa-play"></i></a>`
-                }
-                listItemHTML +=`
-                </div>
-                </div>`;
-
+                  <div class="card mb-2">
+                    <div class="card-body">
+                      <div class="row pb-2">
+                        <div class="col-md-6">
+                          <h5 class="text-start text-primary"><a href="/users/${result._id}">${result.username || 'N/A'}</a></h5>
+                        </div>
+                        <div class="text-end col-md-6">
+                            <a class="mx-1" href="/users/${result.active ? 'disable' : 'enable'}/${result._id}">
+                                <i class="fa-solid ${result.active ? 'fa-pause' : 'fa-play'}"></i>
+                            </a>
+                        </div>
+                      </div>
+                      <div class="row pb-2">
+                        <div class="col-md-12">
+                          <div class="text-start"><b>Username:</b> ${result.username || 'N/A'}</div>
+                          <div class="text-start"><b>Email:</b> ${result.email || 'N/A'}</div>
+                        </div>
+                      </div>`;
+              
+              
+              
+                listItemHTML += `</div></div></div></li>`;
+              
                 // Adicionar o HTML à lista de resultados
                 resultsList.innerHTML += listItemHTML;
-            });
-
+              });
+              
 
             const previous = document.getElementById('previous');
             
