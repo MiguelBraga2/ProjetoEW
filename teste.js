4e277da6af06551b2f276d5493d0be3681418208
MiguelBraga2 <114342823+MiguelBraga2@users.noreply.github.com>
2023-07-02 21:43:48 +0100

    Delete teste.js

--- teste.js
+++
@@ -1,8 +0,0 @@
-const processIds = [1, 2, 3, 4, 5];
-const baseUrl = 'https://api.example.com/processes';
-
-// Construir a URL da API com os parâmetros da query string
-const queryParams = `ids=${processIds.join(',')}`;
-const url = `${baseUrl}?${queryParams}`;
-
-console.log(url)
